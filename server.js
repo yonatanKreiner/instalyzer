37ca6fd10e70834e7e5b9da587f12deb8d7a774f
gannash <mbinyaminov@gmail.com>
2018-12-15 18:27:26 +0200

    disabled cors temporarily

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const router = require('./src');
 
 const app = express();
 
 const port = 3001;
 
+app.use(cors());
 app.use('/', router);
 
 app.listen(port, () => {
