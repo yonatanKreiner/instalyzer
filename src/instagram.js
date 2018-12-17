62ee83b8f3d85b834a9794bb4d1e9637d8f01847
gannash <mbinyaminov@gmail.com>
2018-12-17 08:15:57 +0200

    changed getAccount to call 2 apis and return also image and fullname

--- src/instagram.js
+++ src/instagram.js
@@ -11,8 +11,18 @@ const searchAccounts = async (account) => {
 
 const getAccount = async (account) => {
 	try {
-		const res = await axios.post('https://fetcher.igaudit.io/users', {usernames: [account]});
-		return res.data.data[0];
+		const res = await axios.post('https://fetcher.igaudit.io/users', { usernames: [account] });
+		const accountSearch = await searchAccounts(account);
+		const filteredAccount = accountSearch.filter(function (accountItem) { return accountItem.username === account })[0];
+
+		return ({
+			username: filteredAccount.username,
+			full_name: filteredAccount.full_name,
+			avatar_url: filteredAccount.avatar_url,
+			mediaPosts: res.data.data[0].mediaPosts,
+			followingCount: res.data.data[0].followingCount,
+			followerCount: res.data.data[0].followerCount,
+		});
 	} catch (err) {
 		console.error(err.message);
 	}
