9718bb86184d1140176d441c2eebb37489a91ee0
Yonatan Kreiner <yonatankreiner@gmail.com>
2018-12-19 15:36:21 +0200

    update for heroku production

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const router = require('./src');
 
 const app = express();
 
-const port = 3001;
+app.set('port', process.env.PORT || 3001);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', router);
 
-app.listen(port, () => {
-	console.log('listening on ' + port);
+app.listen(app.get('port'), () => {
+	console.log('listening on ' + app.get('port'));
 });
